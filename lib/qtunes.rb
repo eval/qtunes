845a4538c4a47d301dc04faabfd9eae2162d59e8
Gert Goet <gert@thinkcreate.nl>
2011-08-23 11:15:39 +0200

    library and queue

--- lib/qtunes.rb
+++ lib/qtunes.rb
@@ -1,6 +1,6 @@
+require "qtunes/player"
 require "qtunes/version"
 require "qtunes/server"
-require "qtunes/player"
 
 module Qtunes
 end
--- lib/qtunes/server.rb
+++ lib/qtunes/server.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require 'digest/sha2'
 
 module Qtunes
   class Server < Sinatra::Base
@@ -9,7 +10,43 @@ module Qtunes
     set :static, true
 
     get '/' do
-      erb :index
+      @song = PLAYER.file
+      @songs = queue
+
+      erb :songs
     end
+    
+    get '/library' do
+      @song = PLAYER.file
+      @songs = library
+
+      erb :songs
+    end
+
+    get '/add/:id' do
+      PLAYER.enqueue(library[params[:id]])
+
+      redirect '/'
+    end
+
+    get '/remove/:id' do
+      PLAYER.enqueue(library[params[:id]])
+      redirect '/'
+    end
+
+    configure do
+      puts "Configure"
+
+      PLAYER = Qtunes::Player.new
+    end
+
+    protected
+      def queue
+        PLAYER.queue.inject({}){|res,file| res[Digest::SHA256.hexdigest(file)[0,10]]=file; res}
+      end
+
+      def library
+        @library ||= PLAYER.library.inject({}){|res,file| res[Digest::SHA256.hexdigest(file)[0,10]]=file; res}
+      end
   end
 end
